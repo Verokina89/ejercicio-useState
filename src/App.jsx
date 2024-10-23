e6331d25bb6896b995734b624c967858b8ac024a
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-10-23 22:29:47 +0200

    started exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import './App.css'
-
+import { useState } from 'react';  //imprta useState (hook) para gestinr el estado en componentes funcioanles 
+import './App.css';  //importar css paa app
 function App() {
- //Aquí tu código
+    //define 'name' con el valor inicial "sofia" y 'setName' que actualiza el nombre
+    const [name, setName] = useState("Sofia");
+
+    //renderiza jsx
+    return(
+        <div>
+            <h2>Teacher Name: {name} </h2>      {/*muestra el nombre actual*/}
+        
+            {/*nombres de los profesores*/}
+            <ul>
+                <li onClick={() => setName("Data")}>Data</li>
+                <li onClick={() => setName("Reyes")}>Reyes</li>
+                <li onClick={() => setName("Yolanda")}>Yolanda</li> 
+            </ul>
+        </div>
+    );
 }
 
-export default App
+export default App;
