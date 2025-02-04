12970e1ff7fb54b37abfeaf644e8a609385454a6
Veronica Isabel Perez Suarez <veronicaisabelperezsuarez@MacBook-Pro-de-Veronica-2017.local>
2025-02-04 20:03:34 +0100

    practice css and add name in return

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
                 <input type='text' 
                 value={newName} 
                 onChange={(e) => setNewName(e.target.value)} //funcion anonima que cada vez q cmbie el valor de 'newName' lo añada  'setNewName' que a su vez va a valer 'newName'
-                placeholder='add a name'
+                placeholder='add a name please'
                 />
                 <button type='submit'>Add</button>
             </form>
@@ -31,6 +31,7 @@ function App() {
             <ul>
                 <li onClick={() => setName("Data")}>Data</li>
                 <li onClick={() => setName("Reyes")}>Reyes</li>
+                <li onClick={() => setName("Soraya")}>Soraya</li>
                 <li onClick={() => setName("Yolanda")}>Yolanda</li> 
             </ul>
         </div>
@@ -38,49 +39,3 @@ function App() {
 }
 
 export default App;
-
-
-
-/*
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-  const [name, setName] = useState('Sofia')
-  const [newName, setNewName] = useState('')
-
-  const hadleNameChange = (profesor) => {
-    setName(profesor)
-  }
-
-  const changeName = (e) => {
-    e.preventDefault()
-    if(newName.trim()) {
-      setName(newName)
-      setNewName('')
-    }
-  }
-  
-  return (
-    <>
-      <h2>Teacher name: {name}</h2>
-      <form onSubmit={changeName}>
-        <input 
-          type='text' 
-          value={newName} 
-          onChange={(e) => setNewName(e.target.value)}          
-          placeholder='add a name'
-        />
-        <button type='submit'>Add</button>
-      </form>
-    <ul>
-      <li onClick={() => hadleNameChange("Data")}>Data</li>
-      <li onClick={() => hadleNameChange("Reyes")}>Reyes</li>
-      <li onClick={() => hadleNameChange("Soraya")}>Soraya</li>
-    </ul>
-    </>
-  )
-}
-
-export default App
-*/
